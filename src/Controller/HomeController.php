e4f8718111e2d60b4a4352898b558576530e37a9
Casey Heagerty <cheagerty@gmail.com>
2019-07-15 14:01:15 +0200

    New, functional login box on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,17 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="app_login")
      */
-    public function index()
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
+    }
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/_new_login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
 
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login2", name="app_login2")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -19,6 +19,6 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/_new_login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }
