e2b8bfcca56945f6e254f9270398ecf6b297b48e
Casey Heagerty <cheagerty@gmail.com>
2019-07-16 15:10:47 +0200

    correction of minor bug

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -75,7 +75,7 @@ class StudentController extends AbstractController
                 'kata' => [],
             ]);
         }
-        $codewarsId = c;
+        $codewarsId = $student->getCodewarsId();
         $honor = $codewars->getHonor($codewarsId);
         $kata = $codewars->getKata($codewarsId);
 
