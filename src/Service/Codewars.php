7cebeb5ae2cdf2cd769a8a34790726bc87b4b18c
Casey Heagerty <cheagerty@gmail.com>
2019-07-22 09:50:00 +0200

    update Casey json, codewars service

--- src/Service/Codewars.php
+++ src/Service/Codewars.php
@@ -70,8 +70,12 @@ class Codewars
     public function getLanguages(string $codewarsId) : ?Array
     {
 
-        $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'https://www.codewars.com/api/v1/users/'.$codewarsId.'?access_key=DfJfLt_KyVgikefJ3uzy');
+        //$httpClient = HttpClient::create();
+        //$response = $httpClient->request('GET', 'https://www.codewars.com/api/v1/users/'.$codewarsId.'?access_key=DfJfLt_KyVgikefJ3uzy');
+
+        $jsonFile = file_get_contents('JSON/'.$codewarsId.'.json');
+
+        $json = json_decode($jsonFile, true);
 
         //$statusCode = $response->getStatusCode();
         // $statusCode = 200
@@ -79,8 +83,10 @@ class Codewars
         // $contentType = 'application/json'
         //$content = $response->getContent();
         // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+
+        //$content = $response->toArray();
+        $content = $json;
 
         $ranks = $content['ranks'];
         $languages = $ranks['languages'];
